a819f92de52f0b6a3ae7d0f7afa0e886878c7554
achinthau <achintha@duosoftware.com>
2018-12-13 14:06:55 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -32,10 +32,10 @@ module.exports = {
             "user": "",
             "password": "",
             "mode":"",//instance, cluster, sentinel
-            "sentinels":{
-                "hosts": "",
-                "port":,
-                "name":""
+              "sentinels":{
+                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+                "port":16389,
+                "name":"redis-cluster"
             }
         }
 };
